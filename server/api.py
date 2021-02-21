23f37f26dbdc5354f326bec48a3ca249ae42cd58
didier-rda <didier.rda@gmail.com>
2021-02-21 00:26:01 -0300

    (update)

--- server/api.py
+++ server/api.py
@@ -3,23 +3,20 @@ import os
 import json
 import pickle
 import pandas as pd
+import numpy as np
+
 from flask import request
-from functools import reduce 
+
+from request_predictor import requestPredictor
+
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return {"message": f"ae {os.getenv('MODEL_PATH')}"}
-    
-
 @app.route('/v1/categorize', methods=['POST'])
 def post():
+
     json_file = request.json["products"] #.get('title')
     df = pd.DataFrame().from_dict(json_file)
-    cols = df.columns
-    return {"message": f"{df.shape}"}
-# f_conca = lambda d1, d2: {'title':[d1['title'], d2['title']] , 'query':[d1['query'], d2['query']] , 'concatenated_tags':[d1['concatenated_tags'], d2['concatenated_tags']] }
-# reduce(f_conca, data['products'])
+    req_predictor = requestPredictor(df)
 
-
+    return req_predictor.predict
