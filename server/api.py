8d6f308ddedd1807b5d53b17a20e21c24c41c0e6
didier-rda <didier.rda@gmail.com>
2021-02-25 11:56:13 -0300

    (update) lead witt 400 error on api server

--- server/api.py
+++ server/api.py
@@ -15,8 +15,27 @@ app = Flask(__name__)
 @app.route('/v1/categorize', methods=['POST'])
 def post():
 
-    json_file = request.json["products"] #.get('title')
-    df = pd.DataFrame().from_dict(json_file)
+    try:
+        json_file_list = request.json["products"] #.get('title')
+    
+    except Exception as e:
+        return {'api return': str(e)}, 400
+    
+    for dict_ in json_file_list:
+        try:
+            params = set(dict_.keys())
+        except Exception as e:
+            return {'Error:': str(e)}, 400
+
+        if 'title' not in params or 'query' not in params or 'concatenated_tags' not in params:
+            return {'Error':f"bad request in {dict_}"}, 400
+
+    try:
+        df = pd.DataFrame().from_dict(json_file_list)
+
+    except Exception as e:
+        return {'Error': str(e)}, 400
+
     req_predictor = requestPredictor(df)
 
     return req_predictor.predict
