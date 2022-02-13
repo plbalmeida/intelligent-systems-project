4e1fa2b9a74b1470acc23095f9f0d2589640ecb0
plbalmeida <xpedroalmeidax@gmail.com>
2022-02-12 23:00:45 -0300

    API updated

--- server/api.py
+++ server/api.py
@@ -1,14 +1,19 @@
-import os
-import json
-import pickle
 import pandas as pd
-import numpy as np
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/v1/categorize', methods=['POST'])
-def post():
-    try:
-        json_ = request.json['products']
-        
+def predict():
+    '''API for product category prediction.'''
+
+    json_input = request.json
+    query = pd.DataFrame(json_input)
+    query_transformed = data_engineering.transform(query)
+    prediction = model.predict(query_transformed)
+    return jsonify({'categories': prediction})
+
+if __name__ == '__main__':
+    data_engineering = joblib.load(os.getenv('DATA_ENGINEERING_PATH')) 
+    model = joblib.load(os.getenv('MODEL_PATH'))
+    app.run(port=5000, debug=True)
