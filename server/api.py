51e45a4fbea0f079c056e776ef79a5beef00e772
Pedro Almeida <pedro.almeida@datarain.com.br>
2022-02-20 01:23:51 -0300

    repo update

--- server/api.py
+++ server/api.py
@@ -2,7 +2,7 @@ import json
 import os
 import pickle
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -12,9 +12,9 @@ def predict():
 
     json_input = request.json['products']
     query = pd.DataFrame(json_input)
-    data_engineering = pickle.load(open(os.getenv('DATA_ENGINEERING_PATH'), 'rb'))
+    data_engineering = pickle.load(open(os.getenv('FEATURE_ENGINEERING_PATH'), 'rb'))
     model = pickle.load(open(os.getenv('MODEL_PATH'), 'rb'))
-    query_transformed = data_engineering.transform(query)
+    query_transformed = data_engineering.transform(query['query'])
     prediction = model.predict(query_transformed)
-    return json.dumps({"categories": [prediction[0]]}), 400
-    
+    return json.dumps({"categories": list(prediction)}), 400
+  
---
+++ server/get_json.py
@@ -0,0 +1,14 @@
+import json
+import os
+import pandas as pd
+
+if __name__ == '__main__':
+
+    input = pd.read_csv(open(os.getenv('TEST_SET_PATH')))
+    input_list = input['query'].to_list()
+    input_dict = {"products": []}
+    input_dict["products"] += [{"query": i} for i in input_list]
+    to_json = json.dumps(input_dict)
+
+    with open('input.json', 'w') as f:
+        f.write(to_json)
