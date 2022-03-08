aaba265e0f51d5c0df4b2ee0c5ba3b1dc2469627
Pedro Almeida <xpedroalmeidax@gmail.com>
2022-03-08 09:53:04 -0300

    updating API script

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -20,21 +20,4 @@ def response_200():
     resp = requests.post(url, json=json_data, headers=req.headers)
 
     assert resp.status_code == 200
-
-
-""" @pytest.mark.parametrize('path_400', ('/400/missing_title.json'))
-def test_post_expected_400(path_400):
-    '''test if the expected request code 400 is returned.''' 
     
-    url = "http://0.0.0.0:5000/v1/categorize"	
-    path = 'test/data' + path_400
-    req = urllib.request.Request(url)
-    req.add_header('Content-Type', 'application/json') 
-    #json_file = open('test/data/201/test1.json',)
-    json_file = open(path)
-    json_data = json.load(json_file) 
-    json_file.close()
-    resp = requests.post(url, json=json_data, headers=req.headers)
-
-    assert resp.status_code == 400
- """
